c1d985fa35958b0fd346027ee828965f6d6aa316
At0m1X <TomasHandzel98@gmail.com>
2025-05-30 01:59:07 +0200

    fixing privacy

--- src/components/Client/Footer.jsx
+++ src/components/Client/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   	return (
 		<footer className='p-4 bg-gray-100 text-center text-sm text-gray-600'>
 			&copy {new Date().getFullYear()} Public Advisor. All rights reserved.
-			<a href="/privacy" className="underline hover:text-primary">
+			<a href="http://localhost:5173/privacy" className="underline hover:text-primary">
     			Privacy Policy
   			</a>
 		</footer>
