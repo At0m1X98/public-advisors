0b1873c8a12d429b8e85a4d73acff6bfb72c74f1
At0m1X <TomasHandzel98@gmail.com>
2025-05-30 02:03:15 +0200

    fixing privacy

--- src/components/Client/Footer.jsx
+++ src/components/Client/Footer.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom	'
 
 const Footer = () => {
   	return (
 		<footer className='p-4 bg-gray-100 text-center text-sm text-gray-600'>
 			&copy {new Date().getFullYear()} Public Advisor. All rights reserved.
-			<a href="./privacy" className="underline hover:text-primary">
+			<Link to="/privacy" className="underline hover:text-primary">
     			Privacy Policy
-  			</a>
+  			</Link>
 		</footer>
   	);
 }
